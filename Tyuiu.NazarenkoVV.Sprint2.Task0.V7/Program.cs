930b0a52262e185c6e8f15d68f21e33827a7f2c2
nazarenkovv <vitanazarenko2006@yandex.ru>
2024-11-06 22:31:28 +0500

    Добавьте файлы проекта.

---
+++ Tyuiu.NazarenkoVV.Sprint2.Task0.V7/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.NazarenkoVV.Sprint2.Task0.V7.Lib;
+
+namespace Tyuiu.NazarenkoVV.Sprint2.Task0.V7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            int x = 103;
+            int y = 475;
+            bool[] res = new bool[6];
+            res = ds.GetCompareOperations(x, y);
+
+            Console.WriteLine("********************************************************************************");
+            Console.WriteLine("* Исходные данные                                                              *");
+            Console.WriteLine("********************************************************************************");
+            Console.WriteLine("X = " + x);
+            Console.WriteLine("Y = " + y);
+
+            Console.WriteLine("********************************************************************************");
+            Console.WriteLine("* Результат:                                                                   *");
+            Console.WriteLine("********************************************************************************");
+
+            for (int i=0; i<6; i++)
+            {
+                Console.WriteLine(res[i]);
+            }
+            Console.ReadKey();
+        }
+    }
+}
