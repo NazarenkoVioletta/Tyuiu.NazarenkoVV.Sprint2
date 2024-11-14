797e0d3199c121b3d7394f54efbe7bf32b9bfe8c
nazarenkovv <vitanazarenko2006@yandex.ru>
2024-11-14 11:07:19 +0500

    Task6

--- Tyuiu.NazarenkoVV.Sprint2.Task5.V9.Lib/DataService.cs
+++ Tyuiu.NazarenkoVV.Sprint2.Task5.V9.Lib/DataService.cs
@@ -9,96 +9,97 @@ using tyuiu.cources.programming.interfaces.Sprint2;
 namespace Tyuiu.NazarenkoVV.Sprint2.Task5.V9.Lib
 {
     public class DataService : ISprint2Task5V9
+    {
+        public string FindDateOfNextDay(int m, int n)
         {
-            public string FindDateOfNextDay(int m, int n)
             {
+                if (n == 31)
                 {
-                    if (n == 31)
+                    m += 1;
+                    n -= 30;
+                }
+                else if (n < 31)
+                {
+                    if ((m == 2) && (n == 28))
                     {
                         m += 1;
-                        n -= 30;
+                        n -= 27;
                     }
-                    else if (n < 31)
+                    else if ((m == 4) && (n == 30))
                     {
-                        if ((m == 2) && (n == 28))
-                        {
-                            m += 1;
-                            n -= 27;
-                        }
-                        else if ((m == 4) && (n == 30))
-                        {
-                            m += 1;
-                            n -= 29;
-                        }
-                        else if ((m == 6) && (n == 30))
-                        {
-                            m += 1;
-                            n -= 29;
-                        }
-                        else if ((m == 9) && (n == 30))
-                        {
-                            m += 1;
-                            n -= 29;
-                        }
-                        else if ((m == 11) && (n == 30))
-                        {
-                            m += 1;
-                            n -= 29;
-                        }
-                        else
-                        {
-                            n += 1;
-                        }
-
+                        m += 1;
+                        n -= 29;
                     }
-                    string res;
-                    switch (m)
+                    else if ((m == 6) && (n == 30))
                     {
-                        case 1:
-                            res = "01";
-                            break;
-                        case 2:
-                            res = "02";
-                            break;
-                        case 3:
-                            res = "03";
-                            break;
-                        case 4:
-                            res = "04";
-                            break;
-                        case 5:
-                            res = "05";
-                            break;
-                        case 6:
-                            res = "06";
-                            break;
-                        case 7:
-                            res = "07";
-                            break;
-                        case 8:
-                            res = "08";
-                            break;
-                        case 9:
-                            res = "09";
-                            break;
-                        case 10:
-                            res = "10";
-                            break;
-                        case 11:
-                            res = "11";
-                            break;
-                        case 12:
-                            res = "12";
-                            break;
-                        default:
-                            throw new ArgumentException($"Месяц должен быть от 1 до 12. Значение {m}");
+                        m += 1;
+                        n -= 29;
                     }
-                    string res2;
-                    res2 = n + "." + res;
-                    return res2;
+                    else if ((m == 9) && (n == 30))
+                    {
+                        m += 1;
+                        n -= 29;
+                    }
+                    else if ((m == 11) && (n == 30))
+                    {
+                        m += 1;
+                        n -= 29;
+                    }
+                    else
+                    {
+                        n += 1;
+                    }
 
                 }
+                string res;
+                switch (m)
+                {
+                    case 1:
+                        res = "01";
+                        break;
+                    case 2:
+                        res = "02";
+                        break;
+                    case 3:
+                        res = "03";
+                        break;
+                    case 4:
+                        res = "04";
+                        break;
+                    case 5:
+                        res = "05";
+                        break;
+                    case 6:
+                        res = "06";
+                        break;
+                    case 7:
+                        res = "07";
+                        break;
+                    case 8:
+                        res = "08";
+                        break;
+                    case 9:
+                        res = "09";
+                        break;
+                    case 10:
+                        res = "10";
+                        break;
+                    case 11:
+                        res = "11";
+                        break;
+                    case 12:
+                        res = "12";
+                        break;
+                    default:
+                        throw new ArgumentException($"Месяц должен быть от 1 до 12. Значение {m}");
+                }
+                string res2;
+                res2 = n + "." + res;
+                return res2;
 
             }
+
         }
     }
+}
+    
--- Tyuiu.NazarenkoVV.Sprint2.Task6.V5/Program.cs
+++ Tyuiu.NazarenkoVV.Sprint2.Task6.V5/Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.NazarenkoVV.Sprint2.Task6.V5.Lib;
+
+namespace Tyuiu.NazarenkoVV.Sprint2.Task6.V5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.WriteLine("********************************************************************************");
+            Console.WriteLine("* Исходные данные                                                              *");
+            Console.WriteLine("********************************************************************************");
+            Console.WriteLine("Введите номер карты:   ");
+            int numcart = Convert.ToInt32(Console.ReadLine());
+            string res;
+            if ((numcart <= 6) || (numcart >= 14))
+            {
+                res = "Введено непраивльное значение";
+            }
+            else
+            {
+                res = "Это карта: " + ds.FindCardValue(numcart);
+            }
+            Console.WriteLine("********************************************************************************");
+            Console.WriteLine("* Результат:                                                                   *");
+            Console.WriteLine("********************************************************************************");
+            Console.WriteLine(res);
+            Console.ReadKey();
+        }
+    }
+}
