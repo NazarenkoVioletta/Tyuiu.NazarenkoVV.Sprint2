c604dd9c8138c2a2889895942871970b5359cdc4
nazarenkovv <vitanazarenko2006@yandex.ru>
2024-11-14 13:09:42 +0500

    Task0

--- Tyuiu.NazarenkoVV.Sprint2.Task0.V7.Test/DataServiceTest.cs
+++ Tyuiu.NazarenkoVV.Sprint2.Task0.V7.Test/DataServiceTest.cs
@@ -4,21 +4,16 @@ namespace Tyuiu.NazarenkoVV.Sprint2.Task0.V7.Test
 {
     [TestClass]
     public class DataServiceTest
+    { 
+        public void TestMethod1()
     {
-        public void GetCompareOperations()
-        {
-            DataServiceTest ds = new DataServiceTest();
+            DataService ds = new DataService();
             int x = 103;
             int y = 475;
             bool[] res = new bool[6];
             res = ds.GetCompareOperations(x, y);
             bool[] wait = new bool[6] { true, false, true, true, true, false };
             CollectionAssert.AreEqual(res, wait);
-        }
-
-        private bool[] GetCompareOperations(int x, int y)
-        {
-            throw new NotImplementedException();
         }
     }
 }
